a900e9e1ae6b74cb39183cf21ed71209542b1af5
Blagovest-Todorov <blagovest.stod@abv.bg>
2023-03-01 08:39:26 +0200

    push direct to master

--- RestSharp_GitHub_Tutorial/Program.cs
+++ RestSharp_GitHub_Tutorial/Program.cs
@@ -1,10 +1,13 @@
-﻿namespace RestSharp_GitHub_Tutorial
+﻿using RestSharp;
+
+namespace RestSharp_GitHub_Tutorial
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            var client = new RestClient("https//api.github.com");
         }
     }
 }
