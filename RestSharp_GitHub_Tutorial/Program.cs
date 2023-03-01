ef5b7ad2ec23a2c8fb31f1d921aba75f9eca83c4
Blagovest-Todorov <blagovest.stod@abv.bg>
2023-03-01 08:29:02 +0200

    Add project files.

---
+++ RestSharp_GitHub_Tutorial/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace RestSharp_GitHub_Tutorial
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
