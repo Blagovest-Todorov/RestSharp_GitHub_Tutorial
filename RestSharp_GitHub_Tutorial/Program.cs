a05b9ebdef2d80d2c6351a7c1a36d7d7bb9b15d8
Blagovest-Todorov <blagovest.stod@abv.bg>
2023-03-01 12:48:18 +0200

    added tests

--- RestSharp_GitHub_Tutorial/Program.cs
+++ RestSharp_GitHub_Tutorial/Program.cs
@@ -1,13 +1,23 @@
 ﻿using RestSharp;
+using RestSharp.Authenticators;
+using System.Text.Json;
 
 namespace RestSharp_GitHub_Tutorial
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-            var client = new RestClient("https//api.github.com");
+        public static async Task Main()
+        {            
+            var client = new RestClient("https://contactbook.nakov.repl.co");
+            var url = "/api/contacts";
+             client.Authenticator = new HttpBasicAuthenticator(
+                "testnakov", "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");
+            var request = new RestRequest(url);
+            request.AddBody( new { id = 4, firstName = "Bakery from Rest Sharp"});
+            var response = await client.ExecuteAsync(request, Method.Post);
+
+            var persons = JsonSerializer.Deserialize<List<Issue>>(response.Content);
+             
         }
     }
 }
